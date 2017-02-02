6a4032995f0cbd9f661cf09658b7cb6953d88d92
Raphael Goldfarb <rafman522@Raphaels-MacBook-Pro.local>
2017-02-01 19:29:16 -0500

    having problems with route

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 
+[Route("customer")]
 public class CustomerCareController : Controller
 {
+    
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
